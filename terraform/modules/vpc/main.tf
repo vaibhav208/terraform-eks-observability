b4faac7f2a2ff8ecc03b30356045555ca9df0d7d
vaibhav208 <vaibhav.20555@gmail.com>
2026-06-02 16:30:20 +0530

    final vpc

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -95,3 +95,16 @@ resource "aws_route_table" "private" {
     Name = "${var.project_name}-private-rt"
   }
 }
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
